11eaf930918bc249baf326b76d40390c7d756d68
Kent <164713332@qq.com>
2022-02-08 17:03:01 +0800

    Add gradle project files.

---
+++ JavaLang/src/test/java/ListTest.java
@@ -0,0 +1,12 @@
+import junit.framework.TestCase;
+import org.junit.jupiter.api.Test;
+
+public class ListTest extends TestCase {
+
+    @Test
+    public void testList() {
+        int i = 0;
+        assertEquals(0, i);
+        assertEquals(2, 2);
+    }
+}
