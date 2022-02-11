421c029f0a618ea639d0f5a0333ace94fd068423
Kent <164713332@qq.com>
2022-02-11 14:47:04 +0800

    Add docs.

--- TrySpringFramework/src/main/java/TrySpringFramework/IoCApp.java
+++ TrySpringFramework/src/main/java/TrySpringFramework/IoCApp.java
@@ -9,10 +9,20 @@ public class IoCApp {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("services.xml");
         System.out.println(context);
+
+        // Access a formal bean.
         Service1 s1 = (Service1)context.getBean("service1");
         s1.doSomething();
+
+        // Nesting bean.
         Service2 s2 = (Service2)context.getBean("service2");
         s2.doSomething();
+        // Access service1 through dependency injection.
         s2.service1.doSomething();
+
+        // Access service1 by alias.
+        Service1 s = (Service1)context.getBean("s");
+        s.doSomething();
+
     }
 }
