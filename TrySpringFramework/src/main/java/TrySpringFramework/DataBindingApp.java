7668bc2707d2e13d7f8575803248f8cf9ab1691f
Kent <164713332@qq.com>
2022-02-21 18:25:05 +0800

    Add code for type converting.

--- TrySpringFramework/src/main/java/TrySpringFramework/DataBindingApp.java
+++ TrySpringFramework/src/main/java/TrySpringFramework/DataBindingApp.java
@@ -7,7 +7,18 @@ import org.springframework.beans.BeanWrapperImpl;
 public class DataBindingApp {
     public static void main(String[] args) {
         BeanWrapper person  = new BeanWrapperImpl(new Person());
+
         person.setPropertyValue("name", "Kent");
+        System.out.println(person.getPropertyValue("name"));
+
+        // Type converting is automatically.
+        person.setPropertyValue("name", 123456);
+        System.out.println(person.getPropertyValue("name"));
+
+        person.setPropertyValue("name", '好');
+        System.out.println(person.getPropertyValue("name"));
+
+        person.setPropertyValue("name", true);
         System.out.println(person.getPropertyValue("name"));
     }
 }
