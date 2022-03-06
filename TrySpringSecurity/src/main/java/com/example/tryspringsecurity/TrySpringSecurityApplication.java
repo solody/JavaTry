d71e8d18fedb673cfa55e09177bcae1f91fb861b
Kent <164713332@qq.com>
2022-03-06 23:18:15 +0800

    Add security codes.

---
+++ TrySpringSecurity/src/main/java/com/example/tryspringsecurity/TrySpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.tryspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrySpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrySpringSecurityApplication.class, args);
+    }
+
+}
---
+++ TrySpringSecurity/src/main/java/com/example/tryspringsecurity/config/WebSecurityConfig.java
@@ -0,0 +1,32 @@
+package com.example.tryspringsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withDefaultPasswordEncoder().username("user").password("password").roles("USER").build());
+        return manager;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorize -> authorize
+                        .anyRequest().authenticated()
+                )
+                .formLogin(withDefaults())
+                .httpBasic(withDefaults());
+    }
+}
