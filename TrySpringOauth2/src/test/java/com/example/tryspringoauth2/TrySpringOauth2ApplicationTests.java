6ff0449793354ec3e29e27345d4d96cfd718d598
Kent <164713332@qq.com>
2022-03-06 23:33:56 +0800

    Add Oauth2 codes

---
+++ TrySpringOauth2/src/test/java/com/example/tryspringoauth2/TrySpringOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tryspringoauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrySpringOauth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
