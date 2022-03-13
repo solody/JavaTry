1984b1193e4bf6166a6a01855f41539bab14dad4
Kent Young <kent@upsmobi.ubuntu>
2022-03-13 22:24:44 +0800

    Add TryHibernate.

---
+++ TryHibernate/src/main/java/com/example/tryhibernate/entity/People.java
@@ -0,0 +1,40 @@
+package com.example.tryhibernate.entity;
+
+public class People {
+    private Long id;
+    private String name;
+    private int age;
+
+    public People() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public People(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
---
+++ TryMyBatis/src/main/java/com/example/trymybatis/MyBatisApp.java
@@ -0,0 +1,16 @@
+package com.example.trymybatis;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MyBatisApp {
+    public static void main(String[] args) throws IOException {
+        String resource = "com/example/trymybatis/mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+    }
+}
