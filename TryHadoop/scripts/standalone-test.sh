fa95a262e5c7f7d85adb44bb926939126163bac7
Kent Young <kent@upsmobi.ubuntu>
2022-03-25 18:51:54 +0800

    Started up pseudo distributed hadoop cluster.

--- TryHadoop/scripts/standalone-test.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir /scripts/input
-cp /hadoop/etc/hadoop/*.xml /scripts/input
-/hadoop/bin/hadoop jar /hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-3.3.2.jar grep /scripts/input /scripts/output 'dfs[a-z.]+'
-cat /scripts/output/*
