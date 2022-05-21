eb5f12aca910ce452a9f5adac2c765ed3de61d97
kent@legion <164713332@qq.com>
2022-05-21 06:08:36 +0000

    Add TrySpark

--- TrySpark/src/main/java/com/example/tryspark/MySparkTry.java
+++ TrySpark/src/main/java/com/example/tryspark/MySparkTry.java
@@ -8,13 +8,18 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MySparkTry {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         String appName = String.valueOf(MySparkTry.class);
-        String master = "spark://spark:7077";
+        String master = "spark://localhost:7077";
         SparkConf conf = new SparkConf().setAppName(appName).setMaster(master);
         JavaSparkContext sc = new JavaSparkContext(conf);
 
         List<Integer> data = Arrays.asList(1, 2, 3, 4, 5);
         JavaRDD<Integer> distData = sc.parallelize(data);
+
+        while (true) {
+            System.out.println("Running!!!");
+            Thread.sleep(1000L);
+        }
     }
 }
