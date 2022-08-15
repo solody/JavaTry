1392390c5b6576bb5b6759b159e5065be1dd8d8f
kent@legion <164713332@qq.com>
2022-08-15 07:41:48 +0000

    Add test code of empty string testing.

--- TryJava/src/test/java/TryJava/StringTest.java
+++ TryJava/src/test/java/TryJava/StringTest.java
@@ -1,10 +1,22 @@
 package TryJava;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class StringTest {
     @Test
     void test() {
         String[] strings = "a||b".split("\\|\\|");
+    }
+
+    @Test
+    void testIsEmpty() {
+        String nullString = null;
+        String emptyString = "";
+
+        Assertions.assertEquals(true, emptyString.isEmpty());
+        Assertions.assertThrowsExactly(NullPointerException.class, () -> {
+            nullString.isEmpty();
+        });
     }
 }
