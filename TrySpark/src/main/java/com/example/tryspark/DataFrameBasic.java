9c3a54c116bd28ddc6a470f37082b333ded3ef6d
kent@legion <164713332@qq.com>
2022-08-18 05:35:06 +0000

    Add test of dataframe basic operation.

--- TrySpark/src/main/java/com/example/tryspark/DataFrameBasic.java
+++ TrySpark/src/main/java/com/example/tryspark/DataFrameBasic.java
@@ -1,5 +1,6 @@
 package com.example.tryspark;
 
+import org.apache.spark.sql.AnalysisException;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
@@ -11,7 +12,7 @@ public class DataFrameBasic {
     private static String master = "local[16]";
 
     private static Logger logger = Logger.getGlobal();
-    public static void main(String[] args) {
+    public static void main(String[] args) throws AnalysisException {
         SparkSession spark = SparkSession
                 .builder()
                 .appName(appName)
@@ -22,5 +23,34 @@ public class DataFrameBasic {
 
         df.show();
         df.printSchema();
+
+        // Basic operations of dataframe.
+
+        // Select only the "name" column
+        df.select("name").show();
+
+        // Select everybody, but increment the age by 1
+        df.select(df.col("name"), df.col("age").plus(1)).show();
+
+        // Select people older than 21
+        df.filter(df.col("age").gt(21)).show();
+
+        // Count people by age
+        df.groupBy("age").count().show();
+
+        // Register the DataFrame as a SQL temporary view
+        df.createOrReplaceTempView("people");
+
+        // Running sql on a dataframe.
+
+        Dataset<Row> sqlDF = spark.sql("SELECT * FROM people");
+        sqlDF.show();
+
+        // Register the DataFrame as a global temporary view
+        df.createGlobalTempView("people");
+        // Global temporary view is tied to a system preserved database `global_temp`
+        spark.sql("SELECT * FROM global_temp.people").show();
+        // Global temporary view is cross-session
+        spark.newSession().sql("SELECT * FROM global_temp.people").show();
     }
 }
