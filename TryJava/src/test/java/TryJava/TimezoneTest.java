c7d03f30629c8292840e8bd5491c097ab268befd
kent@legion <164713332@qq.com>
2022-08-18 05:34:15 +0000

    Add timezone test.

---
+++ TryJava/src/test/java/TryJava/TimezoneTest.java
@@ -0,0 +1,15 @@
+package TryJava;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.TimeZone;
+
+public class TimezoneTest {
+    @Test
+    void test() {
+        String id = TimeZone.getTimeZone("Asia/Shanghai").getID();
+        id = TimeZone.getTimeZone("UTC").getID();
+        id = TimeZone.getTimeZone("GMT+0:00").getID();
+        id = TimeZone.getTimeZone("GMT+8:00").getID();
+    }
+}
