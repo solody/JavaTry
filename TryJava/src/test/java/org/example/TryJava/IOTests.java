3f5aae4dbbec17e7a86aeea04d44f47d74269273
kent@legion <164713332@qq.com>
2023-05-06 02:11:54 +0000

    Add Input Output Stream api try.

--- TryJava/src/test/java/org/example/TryJava/IOTests.java
+++ TryJava/src/test/java/org/example/TryJava/IOTests.java
@@ -2,8 +2,10 @@ package org.example.TryJava;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class IOTests {
 
@@ -30,5 +32,102 @@ public class IOTests {
         f4.getParentFile().delete();
         f4.getParentFile().getParentFile().delete();
         f4.getParentFile().getParentFile().getParentFile().delete();
+    }
+
+    @Test
+    void testInputStream() throws IOException {
+        // 创建一个FileInputStream对象:
+        // InputStream input = new FileInputStream("./README.md");
+        InputStream input = Files.newInputStream(Paths.get("./README.md"));
+        for (;;) {
+            int n = input.read(); // 反复调用read()方法，直到返回-1
+            if (n == -1) {
+                break;
+            }
+            System.out.println(n); // 打印byte的值
+        }
+        input.close(); // 关闭流
+
+
+        InputStream input1 = null;
+        try {
+            input1 = new FileInputStream("./README.md");
+            int n;
+            while ((n = input1.read()) != -1) { // 利用while同时读取并判断
+                System.out.println(n);
+            }
+        } finally {
+            if (input1 != null) { input1.close(); }
+        }
+
+
+        try (InputStream input2 = new FileInputStream("./README.md")) {
+            int n;
+            while ((n = input2.read()) != -1) {
+                System.out.println(n);
+            }
+        } // 编译器在此自动为我们写入finally并调用close()
+
+
+        try (InputStream input3 = new FileInputStream("./README.md")) {
+            // 定义1000个字节大小的缓冲区:
+            byte[] buffer = new byte[1000];
+            int n;
+            while ((n = input3.read(buffer)) != -1) { // 读取到缓冲区
+                System.out.println("read " + n + " bytes.");
+            }
+        }
+
+
+        byte[] data = { 72, 101, 108, 108, 111, 33 };
+        try (InputStream input4 = new ByteArrayInputStream(data)) {
+            int n;
+            while ((n = input4.read()) != -1) {
+                System.out.println((char)n);
+            }
+        }
+
+
+        String s;
+        try (InputStream input5 = new FileInputStream("./README.md")) {
+            int n;
+            StringBuilder sb = new StringBuilder();
+            while ((n = input5.read()) != -1) {
+                sb.append((char) n);
+            }
+            s = sb.toString();
+        }
+        System.out.println(s);
+    }
+
+    @Test
+    void testOutputStream() throws IOException {
+        OutputStream output = new FileOutputStream("./test.txt");
+        output.write(72); // H
+        output.write(101); // e
+        output.write(108); // l
+        output.write(108); // l
+        output.write(111); // o
+        output.close();
+
+        OutputStream output2 = new FileOutputStream("./test2.txt");
+        output2.write("Hello".getBytes(StandardCharsets.UTF_8)); // Hello
+        output2.close();
+
+
+        byte[] data;
+        try (ByteArrayOutputStream output3 = new ByteArrayOutputStream()) {
+            output3.write("Hello ".getBytes(StandardCharsets.UTF_8));
+            output3.write("world!".getBytes(StandardCharsets.UTF_8));
+            data = output3.toByteArray();
+        }
+        System.out.println(new String(data, StandardCharsets.UTF_8));
+
+        // 读取input.txt，写入output.txt:
+        try (InputStream input4 = new FileInputStream("./test.txt");
+             OutputStream output4 = new FileOutputStream("./test2.txt"))
+        {
+            input4.transferTo(output4); // transferTo的作用是?
+        }
     }
 }
