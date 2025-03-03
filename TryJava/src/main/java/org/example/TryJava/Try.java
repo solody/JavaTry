ee5ec0b947df39ffd9b66ce0d9d03fcff4a1f0be
kent <164713332@qq.com>
2025-03-03 02:01:50 +0000

    Backup try java code.

---
+++ TryJava/src/main/java/org/example/TryJava/Try.java
@@ -0,0 +1,7 @@
+package org.example.TryJava;
+
+public class Try {
+    public static void main(String[] args) throws InterruptedException {
+        Thread.sleep(3600000);
+    }
+}
--- TryProtobuf/src/main/java/com/solody/javatry/Main.java
+++ TryProtobuf/src/main/java/com/solody/javatry/Main.java
@@ -1,9 +1,20 @@
 package com.solody.javatry;
 
+import com.example.tutorial.protos.Person;
+import com.google.protobuf.InvalidProtocolBufferException;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InvalidProtocolBufferException {
+        Person.Builder personBuilder = Person.newBuilder();
+        personBuilder
+                .setId(1)
+                .setEmail("123@123.com");
 
+        Person person = personBuilder.build();
+        Person person2 = Person.parseFrom(person.toByteArray());
+
+        System.out.println(person2.getId());
     }
 }
