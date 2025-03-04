ed125683b78f5411cc622d151a51f76c5b85776a
kent <164713332@qq.com>
2025-03-04 05:33:59 +0000

    Fix logback

--- TryJava/src/main/java/org/example/TryJava/logging/Sel4jLogging.java
+++ TryJava/src/main/java/org/example/TryJava/logging/Sel4jLogging.java
@@ -6,6 +6,8 @@ import org.slf4j.LoggerFactory;
 public class Sel4jLogging {
     static Logger logger = LoggerFactory.getLogger(Sel4jLogging.class);
     public static void main(String[] args) {
+        logger.error("asdfasfd", new Exception("asdfasdf"));
         logger.info("asdfasfd");
+        logger.warn("asdfasfd");
     }
 }
