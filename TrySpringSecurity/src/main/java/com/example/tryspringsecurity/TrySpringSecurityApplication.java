57559377720f1ffc5c914aa5d335c58d716ef3f5
kent <164713332@qq.com>
2026-07-31 08:39:12 +0000

    Fix try spring security to latest versions.

--- TrySpringSecurity/src/main/java/com/example/tryspringsecurity/TrySpringSecurityApplication.java
+++ TrySpringSecurity/src/main/java/com/example/tryspringsecurity/TrySpringSecurityApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TrySpringSecurityApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(TrySpringSecurityApplication.class, args);
     }
 
--- TrySpringSecurity/src/test/java/com/example/tryspringsecurity/TrySpringSecurityApplicationTests.java
+++ TrySpringSecurity/src/test/java/com/example/tryspringsecurity/TrySpringSecurityApplicationTests.java
@@ -2,12 +2,20 @@ package com.example.tryspringsecurity;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @SpringBootTest
 class TrySpringSecurityApplicationTests {
 
     @Test
     void contextLoads() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("user")
+                .build();
+        System.out.println(user.getPassword());
     }
 
 }
