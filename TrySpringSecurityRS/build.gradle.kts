5e3e95919ebe5808997ca9485defae25f20e667a
kent <164713332@qq.com>
2026-08-05 01:52:46 +0000

    Add try spring security resource server.

---
+++ TrySpringSecurityRS/build.gradle.kts
@@ -0,0 +1,37 @@
+import org.gradle.kotlin.dsl.testImplementation
+
+plugins {
+    java
+    id("org.springframework.boot") version "4.1.0"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.solody"
+version = "0.0.1-SNAPSHOT"
+description = "TrySpringSecurityRS"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(26)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+
+val mockitoAgent = configurations.create(/* name = */ "mockitoAgent")
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
+    testImplementation(libs.mockito)
+    mockitoAgent(libs.mockito) { isTransitive = false }
+}
+
+tasks.withType<Test> {
+    jvmArgs.add("-javaagent:${mockitoAgent.asPath}")
+    useJUnitPlatform()
+}
---
+++ TrySpringSecurityRS/src/main/java/com/solody/tryspringsecurityrs/TrySpringSecurityRsApplication.java
@@ -0,0 +1,13 @@
+package com.solody.tryspringsecurityrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrySpringSecurityRsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrySpringSecurityRsApplication.class, args);
+    }
+
+}
